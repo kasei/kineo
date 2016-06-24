33db5bf819bbf812dec4dcbbe41f4477b7a3bc65
Gregory Todd Williams <greg@evilfunhouse.com>
2016-06-24 10:20:04 -0700

    Initial Commit

---
+++ Kineo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kineo
+//
+//  Created by Gregory Todd Williams on 6/24/16.
+//  Copyright © 2016 Gregory Todd Williams. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
