1edf591cd6460a5189f06ce3776a2692c3c8e4e5
Gregory Todd Williams <greg@evilfunhouse.com>
2018-03-25 21:08:15 -0700

    Add tests for numeric term constructors.

--- Tests/KineoTests/RDF.swift
+++ Tests/KineoTests/RDF.swift
@@ -1,9 +1,38 @@
-//
-//  RDF.swift
-//  kineo-test
-//
-//  Created by Gregory Todd Williams on 3/25/18.
-//  Copyright © 2018 Gregory Todd Williams. All rights reserved.
-//
-
+import XCTest
 import Foundation
+import Kineo
+
+// swiftlint:disable type_body_length
+class RDFTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testConstructorInteger() {
+        let t = Term(integer: 7)
+        XCTAssertEqual(t.value, "7")
+    }
+    
+    func testConstructorDecimal() {
+        let t = Term(decimal: 7.1)
+        XCTAssertEqual(t.value, "7.100000")
+    }
+    
+    func testConstructorFloat() {
+        let t = Term(float: -70.1)
+        XCTAssertEqual(t.value, "-7.010000E+01")
+    }
+    
+    func testConstructorDouble() {
+        let t = Term(double: 700.1)
+        XCTAssertEqual(t.value, "7.001000E+02")
+    }
+
+}
