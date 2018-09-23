e7aada0976cc8f73f740feb92a8c8ee819bf7141
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-22 21:07:18 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 		.library(name: "Kineo", targets: ["Kineo"]),
 	],    
     dependencies: [
-		.package(url: "https://github.com/kasei/swift-sparql-syntax.git", from: "0.0.72"),
+		.package(url: "https://github.com/kasei/swift-sparql-syntax.git", from: "0.0.73"),
 		.package(url: "https://github.com/kasei/swift-serd.git", from: "0.0.3"),
 		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
         .package(url: "https://github.com/kasei/URITemplate.git", from: "2.0.9")
