9dcc48d621f1d05aa91d8da9db35a88345fbafba
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-25 09:02:39 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 		.package(url: "https://github.com/kasei/swift-sparql-syntax.git", from: "0.0.73"),
 		.package(url: "https://github.com/kasei/swift-serd.git", from: "0.0.3"),
 		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
-        .package(url: "https://github.com/kasei/URITemplate.git", from: "2.0.9")
+        .package(url: "https://github.com/kasei/URITemplate.git", from: "2.0.10")
     ],
     targets: [
     	.target(
