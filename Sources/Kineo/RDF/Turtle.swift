dc5bd45991874779d863e7faa558638471a59d5b
Gregory Todd Williams <greg@evilfunhouse.com>
2018-11-06 12:13:26 -0800

    Improved Turtle serialization.

--- Sources/Kineo/RDF/Turtle.swift
+++ Sources/Kineo/RDF/Turtle.swift
@@ -9,6 +9,12 @@
 import Foundation
 import SPARQLSyntax
 
+enum TriplePosition {
+    case subject
+    case predicate
+    case object
+}
+
 extension String {
     static let unicodeScalarsNeedingTurtleIRIEscaping : Set<UnicodeScalar> = {
         var charactersNeedingEscaping = Set<UnicodeScalar>()
@@ -87,9 +93,12 @@ extension String {
 }
 
 extension Term {
-    func turtleData(usingPrefixes prefixes: [String:Term]? = nil) -> Data? {
+    func turtleString(usingPrefixes prefixes: [String:Term]? = nil, for position: TriplePosition = .object) -> String {
         switch self.type {
         case .iri:
+            if position == .predicate && self.value == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" {
+                return "a"
+            }
             let v = self.value
             if let prefixes = prefixes {
                 for (k, ns) in prefixes {
@@ -108,24 +117,34 @@ extension Term {
                                 continue
                             }
                             if case .prefixname(_) = token {
-                                return prefixname.data(using: .utf8)
+                                return prefixname
                             }
                         }
                     }
                 }
             }
-            return "<\(v.turtleIRIEscaped)>".data(using: .utf8)
+            return "<\(v.turtleIRIEscaped)>"
         case .blank:
-            return "_:\(self.value)".data(using: .utf8)
+            return "_:\(self.value)"
         case .language(let l):
-            return "\"\(value.turtleStringEscaped)\"@\(l)".data(using: .utf8)
+            return "\"\(value.turtleStringEscaped)\"@\(l)"
         case .datatype(.integer):
-            return "\(Int(self.numericValue))".data(using: .utf8)
+            return "\(Int(self.numericValue))"
         case .datatype(.string):
-            return "\"\(value.turtleStringEscaped)\"".data(using: .utf8)
+            return "\"\(value.turtleStringEscaped)\""
         case .datatype(let dt):
-            return "\"\(value.turtleStringEscaped)\"^^<\(dt.value)>".data(using: .utf8)
+            return "\"\(value.turtleStringEscaped)\"^^<\(dt.value)>"
         }
+    }
+    
+    func printTurtleString<T: TextOutputStream>(to stream: inout T, usingPrefixes prefixes: [String:Term]? = nil, for position: TriplePosition = .object) {
+        let s = turtleString(usingPrefixes: prefixes, for: position)
+        stream.write(s)
+    }
+    
+    func turtleData(usingPrefixes prefixes: [String:Term]? = nil, for position: TriplePosition = .object) -> Data? {
+        let s = turtleString(usingPrefixes: prefixes, for: position)
+        return s.data(using: .utf8)
     }
 }
 
@@ -153,12 +172,53 @@ open class TurtleSerializer : RDFSerializer {
     }
     
     public func serialize<T: TextOutputStream, S: Sequence>(_ triples: S, to stream: inout T) throws where S.Element == Triple {
-        let data = try serialize(triples)
-        if let string = String(data: data, encoding: .utf8) {
-            stream.write(string)
-        } else {
-            throw SerializationError.encodingError("Failed to encode triples as utf-8")
+        if prefixes.count > 0 {
+            for (k, t) in prefixes {
+                stream.write("@prefix \(k): ")
+                t.printTurtleString(to: &stream)
+                stream.write(" .\n")
+            }
+            stream.write("\n")
         }
+        
+        let subjects = Dictionary(grouping: triples) { $0.subject }
+        for (i, s) in subjects.keys.sorted().enumerated() {
+            let triples = subjects[s]!
+            if i > 0 {
+                stream.write("\n")
+            }
+            try serialize(triples, forSubject: s, toStream: &stream)
+        }
+    }
+    
+    private func serialize<T: TextOutputStream, S: Sequence>(_ triples: S, forPredicate predicate: Term, toStream stream: inout T) throws where S.Element == Triple {
+        let termString = predicate.turtleString(usingPrefixes: prefixes, for: .predicate)
+        stream.write(termString)
+        stream.write(" ")
+        
+        let objects = triples.map { $0.object }
+        for (i, o) in objects.sorted().enumerated() {
+            let termString = o.turtleString(usingPrefixes: prefixes, for: .object)
+            if i > 0 {
+                stream.write(", ")
+            }
+            stream.write(termString)
+        }
+    }
+    
+    private func serialize<T: TextOutputStream, S: Sequence>(_ triples: S, forSubject subject: Term, toStream stream: inout T) throws where S.Element == Triple {
+        let preds = Dictionary(grouping: triples) { $0.predicate }
+        let termString = subject.turtleString(usingPrefixes: prefixes, for: .subject)
+        stream.write(termString)
+        stream.write(" ")
+        for (i, p) in preds.keys.sorted().enumerated() {
+            let t = preds[p]!
+            if i > 0 {
+                stream.write(" ;\n\t")
+            }
+            try serialize(t, forPredicate: p, toStream: &stream)
+        }
+        stream.write(" .\n\n")
     }
     
     private func serialize<S: Sequence>(_ triples: S, forPredicate predicate: Term, to data: inout Data) throws where S.Element == Triple {
@@ -167,7 +227,7 @@ open class TurtleSerializer : RDFSerializer {
         }
         data.append(termData)
         data.append(0x20)
-
+        
         let objects = triples.map { $0.object }
         for (i, o) in objects.sorted().enumerated() {
             guard let termData = o.turtleData(usingPrefixes: prefixes) else {
