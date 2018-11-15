fd40204ebcb2960c2f3628179f60f40c944aa67d
Gregory Todd Williams <greg@evilfunhouse.com>
2018-11-15 09:12:51 -0800

    Moved RDFSerializer and RDFParser protocols to SPARQLSyntax package.

--- Sources/Kineo/RDF/RDF.swift
+++ Sources/Kineo/RDF/RDF.swift
@@ -15,21 +15,6 @@ enum RDFTriplePosition {
     case object
 }
 
-public protocol RDFSerializer {
-    init()
-    var canonicalMediaType: String { get }
-    func serialize<S: Sequence>(_ triples: S) throws -> Data where S.Element == Triple
-    func serialize<T: TextOutputStream, S: Sequence>(_ triples: S, to: inout T) throws where S.Element == Triple
-}
-
-public typealias TripleHandler = (Term, Term, Term) -> Void
-public protocol RDFParser {
-    init()
-    var mediaTypes: Set<String> { get }
-    func parse(string: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> Int
-    func parseFile(_ filename: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> Int
-}
-
 public class RDFSerializationConfiguration {
     public struct ParserContext: RDFParser {
         public var mediaTypes: Set<String>
