ff30a287abd1f57211c3dedf8e9a651743805dfd
Gregory Todd Williams <greg@evilfunhouse.com>
2019-01-30 15:47:17 -0800

    Updated package requirement for SPARQLSyntax 0.0.88.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 		.library(name: "Kineo", targets: ["Kineo"]),
 	],    
     dependencies: [
-		.package(url: "https://github.com/kasei/swift-sparql-syntax.git", .upToNextMinor(from: "0.0.87")),
+		.package(url: "https://github.com/kasei/swift-sparql-syntax.git", .upToNextMinor(from: "0.0.88")),
 		.package(url: "https://github.com/kasei/swift-serd.git", .upToNextMinor(from: "0.0.3")),
 		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
         .package(url: "https://github.com/kasei/URITemplate.git", .upToNextMinor(from: "2.0.10")),
