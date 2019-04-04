c1b46c14fca1e39f598668d6a41e253fb26684e5
Gregory Todd Williams <greg@evilfunhouse.com>
2019-04-04 11:05:31 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		.package(url: "https://github.com/kasei/swift-serd.git", .upToNextMinor(from: "0.0.3")),
 		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.0.0")),
 		.package(url: "https://github.com/kasei/URITemplate.git", .upToNextMinor(from: "2.0.10")),
-		.package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.5"),
+		.package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMinor(from: "0.11.5")),
     ],
     targets: [
     	.target(
