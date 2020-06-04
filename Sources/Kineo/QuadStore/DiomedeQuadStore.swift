c7b533eec32c550e0d2bcf2ed87f7d0b65ed105d
Gregory Todd Williams <greg@evilfunhouse.com>
2020-06-04 14:42:53 -0700

    Update version of Diomede.

--- Sources/Kineo/QuadStore/DiomedeQuadStore.swift
+++ Sources/Kineo/QuadStore/DiomedeQuadStore.swift
@@ -9,12 +9,4 @@ import Foundation
 import SPARQLSyntax
 import DiomedeQuadStore
 
-extension DiomedeQuadStore: MutableQuadStoreProtocol {
-    public func results(matching pattern: QuadPattern) throws -> AnyIterator<SPARQLResult<Term>> {
-        let bindings = try self.bindings(matching: pattern)
-        let results = bindings.lazy.map {
-            SPARQLResult<Term>(bindings: $0)
-        }
-        return AnyIterator(results.makeIterator())
-    }
-}
+extension DiomedeQuadStore: MutableQuadStoreProtocol {}
